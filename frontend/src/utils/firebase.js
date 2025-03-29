36a0efa0b840a3ecb73407fe2cb7f9ec1afb5aee
Yash Talathi <yashtalathi10th@gmail.com>
2025-03-29 18:17:21 +0530

    final one

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,35 +1,31 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth,GoogleAuthProvider, signInWithPopup} from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyB7ZU-_PUnEsRNVAhJ5HvFQ3vVCkJYiXYA",
-  authDomain: "y-blogs.firebaseapp.com",
-  projectId: "y-blogs",
-  storageBucket: "y-blogs.firebasestorage.app",
-  messagingSenderId: "761950884250",
-  appId: "1:761950884250:web:8b373b850183b16c21ea23",
-  measurementId: "G-L8YXTQK11P"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth=getAuth(app)
-const provider=new GoogleAuthProvider();
-
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export async function googleAuth() {
-    try {
-        let data = await signInWithPopup(auth,provider);
-        // console.log(data);
-        return data;
-    }catch (error) {
-        console.log(error );
-    }
+  try {
+    let data = await signInWithPopup(auth, provider);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+export { auth, provider };
